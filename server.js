8d069b9aee89cd63a26086d008ebc3023fde095d
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-07 11:24:45 +0530

    feat: updating port for erroe resolution

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
