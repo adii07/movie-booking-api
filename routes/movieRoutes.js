bc6b628df1c441c216046233679af6722705f749
Aditya Sharma <adityasharma5119@gmail.com>
2025-09-17 22:54:41 +0530

    api to get search by movie name

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -12,5 +12,15 @@ router.post("/", addMovie);
 router.get("/", getMovies);
 router.get("/:id", getMovieById);
 router.delete("/:id", deleteMovie);
+router.get("/search", async (req, res) => {
+  const { q } = req.query;
+  if (!q) return res.status(400).json({ msg: "Query missing" });
+  try {
+    const movies = await Movie.find({ title: { $regex: q, $options: "i" } });
+    res.json(movies);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
